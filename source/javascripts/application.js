d3726fad8261066180b128ae813d6dd7f4c81ebb
Alex Coles <alex@alexbcoles.com>
2013-03-27 20:52:15 +0100

    Remove unusued map, TypeKit references
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require "lib/underscore"
 //= require "lib/jquery-1.9.1.min.js"
 //= require "lib/jquery.timeago"
-//= require "lib/maps.js"
 //= require "lib/jquery.responsive_images"
 //= require "lib/twitter_feed"
 //= require "initializer"
